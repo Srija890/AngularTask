16387f6dd8ce16254d094aacc35b6c22f61c1e43
srija <srijak890@gmail.com>
2019-08-13 16:31:20 +0530

    MuzixAngular

---
+++ MuzixAngular (2)/MuzixAngular/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {MuzixService} from '../muzix.service';
+import { Result } from '../modals/Result';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public tracks: Result;
+
+  constructor(private muzixService : MuzixService) { }
+
+  ngOnInit() {
+  }
+
+  
+}
